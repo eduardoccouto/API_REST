d7e01610dd30cda0052ac44265e144d09c3ef4ab
Eduardo Couto <coutoeduardo858@gmail.com>
2024-11-11 11:31:28 -0300

    multer config

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,4 +1,3 @@
-
 import multer from 'multer';
 import { extname, resolve } from 'path';
 
@@ -6,7 +5,16 @@ import { extname, resolve } from 'path';
 
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
+
+
 export default {
+  fileFilter: (req, file, cb) => {
+    if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg'){
+      return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG'));
+    }
+    return cb(null, true);
+  },
+
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,7 +1,24 @@
-class FotoController {
-  async store(req, res){
 
-    res.json(req.file);
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
+class FotoController {
+  async store(req, res) {
+
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+
+        });
+      }
+
+      return res.json(req.file);
+    });
+
+
   }
 }
 
